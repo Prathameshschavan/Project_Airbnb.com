dc5b70c770a079071222e2ab13ccc12c35e3ac12
niki097 <nikitasaini09@gmail.com>
2023-01-04 22:49:35 +0530

    nn

---
+++ COMPONENTS/components.js
@@ -0,0 +1,18 @@
+const Header=()=>{
+    return  `<div><a href="navbar.html"><img id="navbarlogo" src="../img/long-logo.png" alt=""></a></div>
+    <div class="searchbar">
+    <div id="searchbartext">Anywhere</div>
+    <div id="searchbartext">Any Week</div>
+    <div id="searchbartext2">Add guests</div>
+    <div class="search-icon-div">
+    <span id="search-icon"><i class="fas fa-search"></i>
+    </div>
+    </div>
+    <div class="profile-container">
+    <div class="airbnb-your">Airbnb your home</div>
+    <div class="airbnb-your"><i class="fa-solid fa-globe"></i></div>
+    <div id="plofilediv"></div>
+    </div>
+   `;
+}
+export default Header;
