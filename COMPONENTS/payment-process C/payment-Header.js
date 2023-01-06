95cb66a425b57a215e0027d5080ce60122e373aa
AnujPancholiGithub <helpeventanuj@gmail.com>
2023-01-06 20:21:15 +0530

    Payment Methods Added

---
+++ COMPONENTS/payment-process C/payment-Header.js
@@ -0,0 +1,8 @@
+const paymentHeader = function () {
+    return `<div>
+        <a href="https://www.airbnb.co.in/">   <img src="https://imgpile.com/images/dwDX2S.png" alt=""></a>     
+            </div>
+`
+}
+
+export default paymentHeader;
