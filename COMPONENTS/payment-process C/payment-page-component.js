a91169476c5ff08c0bcd510e3d395b577a84bf9f
AnujPancholiGithub <helpeventanuj@gmail.com>
2023-01-07 20:43:26 +0530

    Payment Functionality in final stage

--- COMPONENTS/payment-process C/payment-page-component.js
+++ COMPONENTS/payment-process C/payment-page-component.js
@@ -14,13 +14,15 @@ const paymentMethod = () => {
                             <div id="Coupon-code-div-ID">
 
                                 <input type="text" id="Coupon-code-input-ID" placeholder="Enter Your Save Code">
+                               
                             </div>
+                                   <div> <button id="have-a-promo-code-a-ID">Add Promo Code</button></div>
                             <div>
                                 <img src="https://imgpile.com/images/dRC3sh.png" alt="">
                             </div>
 
-                            <div id="Confirm&Pay-button-div-ID">
-                                <a href="creditCard.html"><button id="ConfirmPayment-buttom-ID">Confirm & Pay</button></a>
+                            <div id="Confirm&Pay-button-div-ID" >
+                                <a href="creditCard.html"> <button id="ConfirmPayment-buttom-ID" >Confirm & Pay</button></a>
                             </div>
 
                         </div>`
