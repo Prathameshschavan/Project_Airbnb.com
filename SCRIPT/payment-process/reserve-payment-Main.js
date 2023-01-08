382b33e3a34a0a7bd54bf82a9bad0a232fc62aa7
AnujPancholiGithub <helpeventanuj@gmail.com>
2023-01-08 16:43:14 +0530

    NEW changes

--- SCRIPT/payment-process/reserve-payment-Main.js
+++ SCRIPT/payment-process/reserve-payment-Main.js
@@ -1,7 +1,7 @@
 
-var bookingData = JSON.parse(localStorage.getItem());
+// var bookingData = JSON.parse(localStorage.getItem());s
 
-// var bookingData = [{ "image1": "https://a0.muscache.com/im/pictures/6db44cb4-4c96-4ff3-b76d-d1af4cae05b8.jpg?im_w=960", "image2": "https://a0.muscache.com/im/pictures/5e737504-c46e-43ae-9fc0-8ff2aa69b9ef.jpg?im_w=480", "image3": "https://a0.muscache.com/im/pictures/223b7b4d-0132-4ba0-93cd-fa431282953a.jpg?im_w=480", "image4": "https://a0.muscache.com/im/pictures/5a9cdcd0-1786-45fc-8012-196b3e49e582.jpg?im_w=480", "image5": "https://a0.muscache.com/im/pictures/53db79a7-3d0d-47b0-a1a2-9343a6f234c0.jpg?im_w=480", "name": "Klein Kussewitz, Germany", "dist": "Built in 1945", "date": "5-10 Jan", "price": "₹23,153 per night", }]
+var bookingData = [{ "image1": "https://a0.muscache.com/im/pictures/6db44cb4-4c96-4ff3-b76d-d1af4cae05b8.jpg?im_w=960", "image2": "https://a0.muscache.com/im/pictures/5e737504-c46e-43ae-9fc0-8ff2aa69b9ef.jpg?im_w=480", "image3": "https://a0.muscache.com/im/pictures/223b7b4d-0132-4ba0-93cd-fa431282953a.jpg?im_w=480", "image4": "https://a0.muscache.com/im/pictures/5a9cdcd0-1786-45fc-8012-196b3e49e582.jpg?im_w=480", "image5": "https://a0.muscache.com/im/pictures/53db79a7-3d0d-47b0-a1a2-9343a6f234c0.jpg?im_w=480", "name": "Klein Kussewitz, Germany", "dist": "Built in 1945", "date": "5-10 Jan", "price": "₹23,153 per night", }]
 
 
 displayBookingDetails(bookingData);
@@ -29,8 +29,6 @@ function displayBookingDetails(bookingData) {
 
 
     });
-
-
 
 }
 
